7e8dc25160229d8c9dcd9d076860236a621f907e
johnnychen94 <johnnychen94@users.noreply.github.com>
2022-07-25 14:50:37 +0000

    deploy: 3d69a8c15fe9c4afb4d4988de67154e4faa67cf7

--- extra/info.js
+++ extra/info.js
@@ -1,4 +1,4 @@
-const buildmessage=`built at Mon Jul 25 14:39:40 2022`
+const buildmessage=`built at Mon Jul 25 14:50:29 2022`
 const page_foot=`Powered by <a href='https://github.com/JuliaRoadmap/DoctreePages.jl'>DoctreePages.jl</a> and its dependencies.`
 const tar_css=`css`
 const filesuffix=`.html`
